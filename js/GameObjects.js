d2eac75668be6c2723e1146e9c89c27561db26e1
kostDev <kostyaboy777@gmail.com>
2018-03-02 00:28:55 +0200

    Collision Add

---
+++ js/GameObjects.js
@@ -0,0 +1,13 @@
+let collisionCheck = function(ch) {
+    let state = false;
+    switch(ch) {
+        case "w":
+        case "f":
+            break;
+        case "g":
+            state = true;
+            break;
+    } 
+    
+    return state;
+}
