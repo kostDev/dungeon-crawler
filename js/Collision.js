026a831400e351b9a11ed96a7349cc5b8780bf81
kostDev <kostyaboy777@gmail.com>
2018-03-03 01:18:33 +0200

    Add check position and play sound event

--- js/Collision.js
+++ js/Collision.js
@@ -1,12 +1,40 @@
 let collisionCheck = function(ch) {
-    let state = false;
+    let obj = {
+        state : false,
+        simbol : ch,
+    };
+
     switch(ch) {
-        case "w":
-        case "f":
-            break;
         case "g":
-            state = true;
+            obj.state = true;
             break;
-    } 
-    return state;
+    }
+
+    return obj;
+}
+
+// data -> state && simbol, p - Player object
+let checkPosition = function(p) {
+    changeBlocks('f');
+    changeBlocks('c');
+}
+// char - name of block
+let changeBlocks = function(simbol) {
+    [
+        Levels[Player.level].gMap[Player.posY][Player.posX-1],
+        Levels[Player.level].gMap[Player.posY][Player.posX+1],
+        Levels[Player.level].gMap[Player.posY-1][Player.posX],
+        Levels[Player.level].gMap[Player.posY+1][Player.posX]
+    ].some(el =>{
+        if(el == simbol) Sounds.get(simbol).play();
+    });
+
+    Levels[Player.level].gMap[Player.posY][Player.posX-1] == simbol ? 
+        Levels[Player.level].gMap[Player.posY][Player.posX-1] = "g" : null;
+    Levels[Player.level].gMap[Player.posY][Player.posX+1] == simbol  ?
+        Levels[Player.level].gMap[Player.posY][Player.posX+1] = "g" : null;
+    Levels[Player.level].gMap[Player.posY-1][Player.posX] == simbol ? 
+        Levels[Player.level].gMap[Player.posY-1][Player.posX] = "g" : null;
+    Levels[Player.level].gMap[Player.posY+1][Player.posX]  == simbol  ?
+        Levels[Player.level].gMap[Player.posY+1][Player.posX] = "g" : null;
 }
